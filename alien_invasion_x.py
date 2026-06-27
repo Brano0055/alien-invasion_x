d5e645c0de865ace961e8fe087c88a700d238da0
Brano0055 <blackwolf@freenet.de>
2026-06-27 21:28:06 +0200

    Add game settings

--- alien_invasion_x.py
+++ alien_invasion_x.py
@@ -1,6 +1,7 @@
 
 import sys 
 import pygame
+from settings import Settings
 
 class AlienInvasionX():
     """Overall class to manage game assets and behavior."""
@@ -9,9 +10,9 @@ class AlienInvasionX():
         """Initialize the game and create game resources."""
 
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion X")
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -23,7 +24,7 @@ class AlienInvasionX():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
             pygame.display.flip()
 
 if __name__ == "__main__":
